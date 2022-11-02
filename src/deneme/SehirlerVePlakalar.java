c72d782f166fb0538518532ad75199325b7955f9
unknown <namsalmert@gmail.com>
2022-11-02 21:41:16 +0300

    homework

---
+++ src/deneme/SehirlerVePlakalar.java
@@ -0,0 +1,57 @@
+package deneme;
+
+public class SehirlerVePlakalar {
+
+	public static void main(String[] args) {
+		// 1- verilen sehirler arrayinde ";" dan öncesini ayırıyoruz(split())
+		// ayırdğımız bu veriyi yeni bir arraye atıyoruz.
+		// ve yeni arrayı ekrana bastırıyoruz,
+
+		// 2- daha sonra
+		// “Adananın plaka kodu 01” örnek çıktışı gibi ekrana tekrar bastırıyoruz
+		// Sehirleri plaka kodlarıyla birlikte bir diziye atayalım daha sonra
+
+		// split()
+		// indexOf()
+		// substring()
+
+		String sehirler = "01-Adana;02-Adıyaman;03-Afyonkarahisar;04-Ağrı;05-Amasya;06-Ankara;"
+				+ "07-Antalya;08-Artvin;09-Aydın;10-Balıkesir;42-Konya";
+		/*
+		System (Adana +  "adlı şehirin plaka kodu:" 01)
+		Adıyaman adlı şehirin plaka kodu: 02
+		Afyonkarahisar adlı şehirin plaka kodu: 03
+		Ağrı adlı şehirin plaka kodu: 04
+		Amasya adlı şehirin plaka kodu: 05
+		Ankara adlı şehirin plaka kodu: 06
+		Antalya adlı şehirin plaka kodu: 07
+		Artvin adlı şehirin plaka kodu: 08
+		Aydın adlı şehirin plaka kodu: 09
+		Balıkesir adlı şehirin plaka kodu: 10
+		Konya adlı şehirin plaka kodu: 42
+		*/
+		
+		String arraySehirler[] =sehirleriAyir(sehirler);
+		for (int i = 0; i < arraySehirler.length; i++) {          //yeni arrayi ekrana yazdırma
+			System.out.println(arraySehirler[i]);
+		}
+		plakalariAyir(arraySehirler);
+		
+
+	} 
+	public static String[] sehirleriAyir(String sehirler) {         
+		    
+		String[] arraySehirler=sehirler.split(";");                     //stringi arraye çevirme
+		return arraySehirler;
+	}
+	public static void plakalariAyir(String[] ayrilcak) {               
+		
+		for(int i = 0; i<ayrilcak.length;i++) {
+			String[] plakaSehirler = ayrilcak[i].split("-");                   //şehirle plakasını ayırma
+			System.out.println(plakaSehirler[1]+" adlı şehrin plaka kodu : "+plakaSehirler[0]);
+		}
+		
+		
+	}
+
+}
